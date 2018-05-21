b0f1683e7312fc36dadff7769549f2f3cf9d666e
Usuario001 <patinogutierrez@icloud.com>
2018-05-21 16:08:30 -0500

    fb callback

---
+++ fb-callback.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jmpg
+ * Date: 21/05/18
+ * Time: 15:18
+ */
+require_once "config.php";
+try{
+    $accessToken = $helper->getAccessToken();
+}catch (\Facebook\Exceptions\FacebookResponseException $e){
+    echo "reponse exception: " . $e->getMessage();
+    die();
+}catch (\Facebook\Exceptions\FacebookSDKException $e){
+    echo "Response SDK exception: " . $e->getMessage();
+    die();
+}
